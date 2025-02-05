33faab99433cf4f3bfbaa07e180588d8d4a2f5fa
RamuAskani <ramu.askani@cestasoft.co.za>
2025-02-05 16:37:05 +0530

    Added page objects package and channels and References feature file

---
+++ src/test/java/smsc_pageobjects/smsc_LoginObjects.java
@@ -0,0 +1,11 @@
+package smsc_pageobjects;
+
+import org.openqa.selenium.By;
+
+public class smsc_LoginObjects {
+    public static final By Login_heading = By.xpath("//img[@alt='Absa Logo']");
+    public static final By Username_txtb = By.xpath("//input[@id=':r0:']");
+    public static final By Password_txtb = By.xpath("//input[@id=':r1:']");
+    public static final By SubmitLogin_btn = By.xpath("//button[@type='submit']");
+    public static final By Loggedin_heading = By.xpath("//img[@alt='logo']");
+}
