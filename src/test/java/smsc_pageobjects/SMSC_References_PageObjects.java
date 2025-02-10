e7a6e45f81539689a010a759a8dcfa821b451806
RamuAskani <ramu.askani@cestasoft.co.za>
2025-02-10 16:32:36 +0530

    Updated References code

--- src/test/java/smsc_pageobjects/SMSC_References_PageObjects.java
+++ src/test/java/smsc_pageobjects/SMSC_References_PageObjects.java
@@ -3,26 +3,26 @@ package smsc_pageobjects;
 import org.openqa.selenium.By;
 
 public class SMSC_References_PageObjects {
-    public static final By referencesSideBar = By.xpath("//p[text()='References']");
-    public static final By createNewReferenceButton = By.xpath("//button[text()='Create New Reference']");
-    public static final By referenceName = By.xpath("//input[@name='name']");
-    public static final By reference = By.xpath("//input[@name='reference']");
-    public static final By partyType = By.xpath("//div[@id='mui-component-select-partyType']");
-    public static final By partyTypeOption = By.xpath("//li[text()='Individual']");
-    public static final By emailAddress = By.xpath("//input[@name='email']");
-    public static final By contactNumber = By.xpath("//input[@name='contact']");
-    public static final By description = By.xpath("//textarea[@name='description']");
+    public static final By referencesSideBar_btn = By.xpath("//p[text()='References']");
+    public static final By createNewReference_btn = By.xpath("//button[text()='Create New Reference']");
+    public static final By referenceName_txtb = By.xpath("//input[@name='name']");
+    public static final By reference_txtb = By.xpath("//input[@name='reference']");
+    public static final By partyType_dropdown = By.xpath("//div[@id='mui-component-select-partyType']");
+    public static final By partyTypeOption_dropdown = By.xpath("//li[text()='Individual']");
+    public static final By emailAddress_txtb = By.xpath("//input[@name='email']");
+    public static final By contactNumber_txtb = By.xpath("//input[@name='contact']");
+    public static final By description_txtb = By.xpath("//textarea[@name='description']");
 
 // Mandatory fields error messages
-    public static final By errorReferenceName = By.xpath("//p[text()='Name is required']");
-    public static final By errorReference = By.xpath("//p[text()='Reference is required']");
-    public static final By errorPartyType = By.xpath("//p[text()='Party Type is required']");
-    public static final By errorEmailAddress = By.xpath("//p[text()='Email is required']");
-    public static final By errorContactNumber = By.xpath("//p[text()='Contact is required']");
-    public static final By errorDescription = By.xpath("//p[text()='Description is required']");
+    public static final By errorReferenceName_txt = By.xpath("//p[text()='Name is required']");
+    public static final By errorReference_txt = By.xpath("//p[text()='Reference is required']");
+    public static final By errorPartyType_txt = By.xpath("//p[text()='Party Type is required']");
+    public static final By errorEmailAddress_txt = By.xpath("//p[text()='Email is required']");
+    public static final By errorContactNumber_txt = By.xpath("//p[text()='Contact is required']");
+    public static final By errorDescription_txt = By.xpath("//p[text()='Description is required']");
 
-    public static final By validateReferenceCreated = By.xpath("//div[@class='main-content-home']/div/div/following-sibling::div/div/table/tbody/tr/td/following-sibling::td/following-sibling::td");
-//    public static final By  = By.xpath("");
+    public static final By validateReferenceCreated_txt = By.xpath("//div[@class='main-content-home']/div/div/following-sibling::div/div/table/tbody/tr/td/following-sibling::td/following-sibling::td");
+    public static final By searchErrorMessage = By.xpath("//td[text()='No Reference found.']");
 //    public static final By  = By.xpath("");
 //    public static final By  = By.xpath("");
 //    public static final By  = By.xpath("");
--- src/test/java/smsc_stepDefinitions/SMSC_Reference.java
+++ src/test/java/smsc_stepDefinitions/SMSC_Reference.java
@@ -1,77 +1,81 @@
 package smsc_stepDefinitions;
 
 import io.cucumber.java.en.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import smsc_accelerators.SMSC_Actions;
+import smsc_accelerators.SMSC_Base;
 import smsc_pageobjects.SMSC_Channels_PageObjects;
 import smsc_pageobjects.SMSC_References_PageObjects;
 import smsc_utility.Logs;
 import smsc_utility.SMSC_ExceptionHandler;
 import smsc_utility.SMSC_Utils;
 
-public class SMSC_Reference {
+public class SMSC_Reference extends SMSC_Base {
     public static String referenceName;
     @Given("User clicks on References on the side bar")
     public void user_clicks_on_references_on_the_side_bar() {
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.referencesSideBar, 10);
-        SMSC_Actions.clickOnElement(SMSC_References_PageObjects.referencesSideBar, "References on the sidebar");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.referencesSideBar_btn, 10);
+        SMSC_Actions.clickOnElement(SMSC_References_PageObjects.referencesSideBar_btn, "References on the sidebar");
     }
 
     @Given("User clicks on Create New Reference button")
     public void user_clicks_on_create_new_reference_button() {
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.createNewReferenceButton, 10);
-        SMSC_Actions.clickOnElement(SMSC_References_PageObjects.createNewReferenceButton, "Create New Reference Button");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.createNewReference_btn, 10);
+        SMSC_Actions.clickOnElement(SMSC_References_PageObjects.createNewReference_btn, "Create New Reference Button");
     }
 
     @Given("User enters Reference Name as {string} on New Reference page")
     public void user_enters_reference_name_as_on_new_reference_page(String string) {
         referenceName = SMSC_Utils.ConfigReader.getProperty("referenceName");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.referenceName, 10);
-        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.referenceName, referenceName, "Reference Name");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.referenceName_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.referenceName_txtb, referenceName, "Reference Name");
     }
 
     @Given("User enters  Reference as {string} on New Reference page")
     public void user_enters_reference_as_on_new_reference_page(String string) {
         String reference = SMSC_Utils.ConfigReader.getProperty("reference");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.reference, 10);
-        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.reference, reference, "Reference");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.reference_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.reference_txtb, reference, "Reference");
     }
 
     @Given("User selects Party Type as {string} on New Reference page")
     public void user_selects_party_type_as_on_new_reference_page(String string) throws Exception {
         String partyType = SMSC_Utils.ConfigReader.getProperty("partyType");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.partyType, 10);
-        SMSC_Actions.ClickViaMouse(SMSC_References_PageObjects.partyType, "Party Type");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.partyTypeOption, 10);
-        SMSC_Actions.ClickViaMouse(SMSC_References_PageObjects.partyTypeOption, partyType);
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.partyType_dropdown, 10);
+        SMSC_Actions.ClickViaMouse(SMSC_References_PageObjects.partyType_dropdown, "Party Type");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.partyTypeOption_dropdown, 10);
+        SMSC_Actions.ClickViaMouse(SMSC_References_PageObjects.partyTypeOption_dropdown, partyType);
     }
 
     @Given("User enters Email Address as {string} on New Reference page")
     public void user_enters_email_address_as_on_new_reference_page(String string) {
         String emailAddress = SMSC_Utils.ConfigReader.getProperty("emailAddress");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.emailAddress, 10);
-        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.emailAddress, emailAddress, "Email Address");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.emailAddress_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.emailAddress_txtb, emailAddress, "Email Address");
     }
 
     @Given("User enters Contact Number as {string} on New Reference page")
     public void user_enters_contact_number_as_on_new_reference_page(String string) {
         String contactNumber = SMSC_Utils.ConfigReader.getProperty("contactNumber");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.contactNumber, 10);
-        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.contactNumber, contactNumber, "Contact Number");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.contactNumber_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.contactNumber_txtb, contactNumber, "Contact Number");
     }
 
     @Given("User enters Description as {string} on New Reference page")
     public void user_enters_description_as_on_new_reference_page(String string) {
         String description = SMSC_Utils.ConfigReader.getProperty("description");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.description, 10);
-        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.description, description, "Description");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.description_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.description_txtb, description, "Description");
     }
 
     @Then("User verifies the created Reference to validate reference is created")
-    public void user_verifies_the_created_reference_to_validate_reference_is_created() {
+    public void user_verifies_the_created_reference_to_validate_reference_is_created() throws Exception {
         referenceName = SMSC_Utils.ConfigReader.getProperty("referenceName");
         SMSC_Actions.waitForElement(SMSC_Channels_PageObjects.search_txtb, 10);
+        SMSC_Actions.ClickViaMouse(SMSC_Channels_PageObjects.search_txtb, "Search box");
         SMSC_Actions.typeInTextBox(SMSC_Channels_PageObjects.search_txtb, referenceName, "Search Box");
-        String validateReferenceCreated = SMSC_Actions.getText(SMSC_References_PageObjects.validateReferenceCreated, "search get text");
+        String validateReferenceCreated = SMSC_Actions.getElementText(SMSC_References_PageObjects.validateReferenceCreated_txt, "search get text");
         System.out.println(validateReferenceCreated);
         if(!validateReferenceCreated.contains(referenceName)){
             SMSC_ExceptionHandler.HandleAssertion("New Reference is not created");
@@ -82,14 +86,19 @@ public class SMSC_Reference {
 
     @Given("User enters Reference Name as {string} on New Reference page for Cancel button")
     public void user_enters_reference_name_as_on_new_reference_page_for_Cancel_button(String string) {
-        referenceName = SMSC_Utils.ConfigReader.getProperty("referenceName");
-        SMSC_Actions.waitForElement(SMSC_References_PageObjects.referenceName, 10);
-        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.referenceName, referenceName, "Reference Name");
+       String referenceNameCancel = SMSC_Utils.ConfigReader.getProperty("referenceNameCancel");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.referenceName_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.referenceName_txtb, referenceNameCancel, "Reference Name");
     }
 
     @Then("User should be redirected to the References page and no changes should be saved")
     public void user_should_be_redirected_to_the_references_page_and_no_changes_should_be_saved() {
-
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.createNewReference_btn, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.createNewReference_btn, "Create New Reference button")){
+            SMSC_ExceptionHandler.HandleAssertion("New Reference is created");
+        }else {
+            Logs.info("New Reference is not created");
+        }
     }
 
     @Given("User clicks on Create button to validate mandatory fields of New Reference")
@@ -100,41 +109,119 @@ public class SMSC_Reference {
 
     @Then("user should see error messages for all the reference mandatory fields")
     public void user_should_see_error_messages_for_all_the_reference_mandatory_fields() {
-
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.errorReferenceName_txt, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.errorReferenceName_txt, "Name is required")){
+            SMSC_ExceptionHandler.HandleAssertion("Name is required element is not visible");
+        }else{
+            Logs.info("Name is required element is visible");
+        }
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.errorReference_txt, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.errorReference_txt, "Reference is required")){
+            SMSC_ExceptionHandler.HandleAssertion("Reference is required element is not visible");
+        }else{
+            Logs.info("Reference is required element is visible");
+        }
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.errorPartyType_txt, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.errorPartyType_txt, "Party Type is required")){
+            SMSC_ExceptionHandler.HandleAssertion("Party Type is required element is not visible");
+        }else{
+            Logs.info("Party Type is required element is visible");
+        }
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.errorEmailAddress_txt, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.errorEmailAddress_txt, "Email is required")){
+            SMSC_ExceptionHandler.HandleAssertion("Email is required element is not visible");
+        }else{
+            Logs.info("Email is required element is visible");
+        }
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.errorContactNumber_txt, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.errorContactNumber_txt, "Contact is required")){
+            SMSC_ExceptionHandler.HandleAssertion("Contact is required element is not visible");
+        }else{
+            Logs.info("Contact is required element is visible");
+        }
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.errorDescription_txt, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.errorDescription_txt, "Description is required")){
+            SMSC_ExceptionHandler.HandleAssertion("Description is required element is not visible");
+        }else{
+            Logs.info("Description is required element is visible");
+        }
     }
 
     @Given("User clicks on Edit button on References page")
     public void user_clicks_on_edit_button_on_references_page() {
-
+        SMSC_Actions.waitForElement(SMSC_Channels_PageObjects.search_txtb, 10);
+        String text = SMSC_Utils.ConfigReader.getProperty("referenceName");
+        SMSC_Actions.typeInTextBox(SMSC_Channels_PageObjects.search_txtb, text, "Search Box");
+        SMSC_Actions.clickOnElement(SMSC_Channels_PageObjects.edit_btn, "Edit button");
     }
 
     @Given("User updates the Description field as {string}")
     public void user_updates_the_description_field_as(String string) {
-
+        String updateDescription = SMSC_Utils.ConfigReader.getProperty("updateReference");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.description_txtb, 10);
+        SMSC_Actions.jsClickOnElement(SMSC_References_PageObjects.description_txtb, "Description");
+        SMSC_Actions.clearTextbox(SMSC_References_PageObjects.description_txtb, "Description");
+        WebElement element = driver.findElement(SMSC_References_PageObjects.description_txtb);
+        element.sendKeys(Keys.CONTROL + "a"); // Select all text
+        element.sendKeys(Keys.BACK_SPACE);
+        SMSC_Actions.typeInTextBox(SMSC_References_PageObjects.description_txtb, updateDescription, "Description");
     }
 
     @Then("User validates the updates of the updated reference")
     public void user_validates_the_updates_of_the_updated_reference() {
-
+        SMSC_Actions.waitForElement(SMSC_Channels_PageObjects.search_txtb, 10);
+        String text = SMSC_Utils.ConfigReader.getProperty("referenceName");
+        SMSC_Actions.typeInTextBox(SMSC_Channels_PageObjects.search_txtb, text, "Search Box");
+        SMSC_Actions.clickOnElement(SMSC_Channels_PageObjects.edit_btn, "Edit button");
+        String fetchedDescriptionText = SMSC_Actions.getElementText(SMSC_References_PageObjects.description_txtb, "Updated Provider");
+        System.out.println(fetchedDescriptionText);
+        String updateDescriptionText = SMSC_Utils.ConfigReader.getProperty("updateReference");
+        System.out.println(updateDescriptionText);
+        if (!fetchedDescriptionText.contains(updateDescriptionText)){
+            SMSC_ExceptionHandler.HandleAssertion("Description is not updated");
+        }else {
+            Logs.info("Description is updated");
+        }
     }
 
     @Given("User enters valid filter criteria in the search box on references page")
     public void user_enters_valid_filter_criteria_in_the_search_box_on_references_page() {
-
+        String searchContent = SMSC_Utils.ConfigReader.getProperty("referenceName");
+        SMSC_Actions.waitForElement(SMSC_Channels_PageObjects.search_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_Channels_PageObjects.search_txtb, searchContent, "Search Box");
     }
 
     @Then("User validates the valid search results on references page")
     public void user_validates_the_valid_search_results_on_references_page() {
-
+        String searchContent = SMSC_Utils.ConfigReader.getProperty("referenceName");
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.validateReferenceCreated_txt, 10);
+        String searchResults = SMSC_Actions.getText(SMSC_References_PageObjects.validateReferenceCreated_txt, "Search validate");
+        if (!searchResults.contains(searchContent)){
+            SMSC_ExceptionHandler.HandleAssertion("Search results not validated");
+        }else{
+            Logs.info("Search results validated");
+        }
     }
 
     @Then("User enters invalid filter criteria in the search box on references page")
     public void user_enters_invalid_filter_criteria_in_the_search_box_on_references_page() {
-
+        String invalidSearch = SMSC_Utils.ConfigReader.getProperty("invalidSearch");
+        SMSC_Actions.waitForElement(SMSC_Channels_PageObjects.search_txtb, 10);
+        SMSC_Actions.typeInTextBox(SMSC_Channels_PageObjects.search_txtb, invalidSearch, "Search Box");
     }
 
     @Then("User validates the error message as {string} on references page")
     public void user_validates_the_error_message_as_on_references_page(String string) {
-
+        SMSC_Actions.waitForElement(SMSC_References_PageObjects.searchErrorMessage, 10);
+        if(!SMSC_Actions.isElementVisible(SMSC_References_PageObjects.searchErrorMessage, "Invalid search")){
+            SMSC_ExceptionHandler.HandleAssertion("No References found error message is not displayed");
+        }else{
+            Logs.info("No References found error message is displayed");
+        }
+        SMSC_Actions.waitForElement(SMSC_Channels_PageObjects.search_txtb, 10);
+        SMSC_Actions.clearTextbox(SMSC_Channels_PageObjects.search_txtb, "Search box");
+        WebElement element = driver.findElement(SMSC_Channels_PageObjects.search_txtb);
+        element.sendKeys(Keys.CONTROL + "a"); // Select all text
+        element.sendKeys(Keys.BACK_SPACE);
     }
 }
