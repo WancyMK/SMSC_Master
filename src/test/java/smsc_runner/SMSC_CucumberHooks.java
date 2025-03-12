312f18cc5b968b9ac5377d64e22a9e57fca56290
Cheepori <Venkatesh.Cheepori@cestasoft.co.za>
2025-03-12 15:44:08 +0530

    New Changes

--- src/test/java/smsc_runner/SMSC_CucumberHooks.java
+++ src/test/java/smsc_runner/SMSC_CucumberHooks.java
@@ -2,6 +2,12 @@ package smsc_runner;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import smsc_accelerators.SMSC_Base;
+import smsc_utility.Logs;
+import smsc_utility.SMSC_ExceptionHandler;
 import smsc_utility.SMSC_Utils;
 import static smsc_accelerators.SMSC_Base.*;
 
@@ -18,7 +24,18 @@ public class SMSC_CucumberHooks {
 
 //    	Everything that needs to be done after every scenario is run
     @After
-    public void cleanup() {
-        tearDown();
+    public void cleanup(Scenario scenario) {
+        try {
+            if(scenario.isFailed()) {
+                final byte[] screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png", "image");
+                System.out.println("After Hook");
+            }
+            Logs.endTestCase();
+            SMSC_Base.tearDown();
+
+        }catch(Exception e) {
+            SMSC_ExceptionHandler.HandleException(e, "Unable to run After scenario");
+        }
     }
 }
